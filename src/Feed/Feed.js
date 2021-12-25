02e485f78dd7ba7bb136d3b2c7e1cf350db66041
Harsh-Manoor <harshmanoor@gmail.com>
2021-12-25 13:01:46 +0530

    post template

--- src/Feed/Feed.js
+++ src/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Feed.css'
 import TweetBox from '../TweetBox/TweetBox'
+import Post from '../Post/Post'
 
 const Feed = () => {
     return (
@@ -13,6 +14,8 @@ const Feed = () => {
 
                 {/* Tweet Box */}
                 <TweetBox />
+
+                <Post />
 
             </div>
         </>
--- src/Post/Post.js
+++ src/Post/Post.js
@@ -1,9 +1,36 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
+import "./Post.css"
 
-const Post = () => {
+const Post = ({displayName,userName,verified,timestamp,text,image,avatar}) => {
     return (
         <>
-               
+            <div className='post'>
+                <div className='post-avatar'>
+                    <Avatar />
+                </div>
+
+                <div className='post-body'>
+                    <div className='post-header'>
+                        <div className='post-header-text'>
+                            <h3>Harsh{" "} <span className='verified-badge'><i class="fas fa-certificate"></i></span> @UserName</h3>
+                        </div>
+
+                        <div className='post-headerDescription'>
+                            <p>Lorum Ipsum hello world</p>
+                        </div>
+                    </div>
+
+                    <img src="https://media.giphy.com/media/Lq5r6qXmb5RUIZGbT0/giphy.gif" alt="post"/>
+                    
+                    <div className='post-footer'>
+                        <i class="far fa-comment-alt"></i>
+                        <i class="fas fa-redo-alt"></i>
+                        <i class="far fa-heart"></i>
+                        <i class="far fa-share-square"></i>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
